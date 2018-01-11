e2bb46ce984254ba88857114995742ad12cbac7c
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-01-11 17:05:18 +0100

    Updated weather icon

--- routes/index.js
+++ routes/index.js
@@ -49,10 +49,12 @@ router.post("/get-current-weather", function(req, res) {
           speech: dataToSend,
           displayText: dataToSend,
           source: "get-current-weather",
-          iconURL:
-            "http://openweathermap.org/img/w/" +
-            weather.weather[0].icon +
-            ".png"
+          data: {
+            iconURL:
+              "http://openweathermap.org/img/w/" +
+              weather.weather[0].icon +
+              ".png"
+          }
         });
       });
     },
